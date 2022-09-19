2af3a0ee683d9a42054f131333c2f2415aff7678
JO-HYEWON <107868634+JO-HYEWON@users.noreply.github.com>
2022-09-19 14:55:00 +0900

    test

--- src/main/java/com/kh/bbang/faq/store/FaqStore.java
+++ src/main/java/com/kh/bbang/faq/store/FaqStore.java
@@ -1,7 +1,6 @@
 package com.kh.bbang.faq.store;
 
 public interface FaqStore {
-	public void test10();
+
 	
-	public void test11();
 }
--- src/main/java/com/kh/bbang/faq/store/logic/FaqStoreLogic.java
+++ src/main/java/com/kh/bbang/faq/store/logic/FaqStoreLogic.java
@@ -4,16 +4,6 @@ import com.kh.bbang.faq.store.FaqStore;
 
 public class FaqStoreLogic implements FaqStore {
 
-	@Override
-	public void test10() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void test11() {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 }
