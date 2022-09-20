a5858cb8b60895163bec2bb4200351bd8ecdd005
Long silverline <long.silverline@gmail.com>
2022-09-20 09:36:53 +0900

    ewview패키지 전체 수정,

--- src/main/java/com/kh/bbang/review/domain/Review.java
+++ src/main/java/com/kh/bbang/review/domain/Review.java
@@ -7,26 +7,25 @@ import java.sql.Date;
  *
  */
 public class Review {
-	private int storeNo;
+	private int reviewNo; 
 	private String userId;
-	private int reviewNo;
+	private int storeNo;
 	private String reviewTitle;
 	private int StarRating;
 	private int ReviewCount;
 	private Date reviewDate;
 	private String reviewContents;
-	private int reviewHeartConut;
+	private int HeartConut;
 	private String reviewStatus;
 	
-	
-	////////////////////////////////////////////////////////////////////////////////////
-	
-	public int getStoreNo() {
-		return storeNo;
+	public Review() {}
+
+	public int getReviewNo() {
+		return reviewNo;
 	}
 
-	public void setStoreNo(int storeNo) {
-		this.storeNo = storeNo;
+	public void setReviewNo(int reviewNo) {
+		this.reviewNo = reviewNo;
 	}
 
 	public String getUserId() {
@@ -37,12 +36,12 @@ public class Review {
 		this.userId = userId;
 	}
 
-	public int getReviewNo() {
-		return reviewNo;
+	public int getStoreNo() {
+		return storeNo;
 	}
 
-	public void setReviewNo(int reviewNo) {
-		this.reviewNo = reviewNo;
+	public void setStoreNo(int storeNo) {
+		this.storeNo = storeNo;
 	}
 
 	public String getReviewTitle() {
@@ -85,12 +84,12 @@ public class Review {
 		this.reviewContents = reviewContents;
 	}
 
-	public int getReviewHeartConut() {
-		return reviewHeartConut;
+	public int getHeartConut() {
+		return HeartConut;
 	}
 
-	public void setReviewHeartConut(int reviewHeartConut) {
-		this.reviewHeartConut = reviewHeartConut;
+	public void setHeartConut(int heartConut) {
+		HeartConut = heartConut;
 	}
 
 	public String getReviewStatus() {
@@ -100,39 +99,28 @@ public class Review {
 	public void setReviewStatus(String reviewStatus) {
 		this.reviewStatus = reviewStatus;
 	}
-	
-	////////////////////////////////////////////////////////////////////////////////////
-	
-	@Override
-	public String toString() {
-		return "Review [storeNo=" + storeNo + ", userId=" + userId + ", reviewNo=" + reviewNo + ", reviewTitle="
-				+ reviewTitle + ", StarRating=" + StarRating + ", ReviewCount=" + ReviewCount + ", reviewDate="
-				+ reviewDate + ", reviewContents=" + reviewContents + ", reviewHeartConut=" + reviewHeartConut
-				+ ", reviewStatus=" + reviewStatus + "]";
-	}
 
-	////////////////////////////////////////////////////////////////////////////////////
-	
-	public Review(int storeNo, String userId, int reviewNo, String reviewTitle, int starRating, int reviewCount,
-			Date reviewDate, String reviewContents, int reviewHeartConut, String reviewStatus) {
+	public Review(int reviewNo, String userId, int storeNo, String reviewTitle, int starRating, int reviewCount,
+			Date reviewDate, String reviewContents, int heartConut, String reviewStatus) {
 		super();
-		this.storeNo = storeNo;
-		this.userId = userId;
 		this.reviewNo = reviewNo;
+		this.userId = userId;
+		this.storeNo = storeNo;
 		this.reviewTitle = reviewTitle;
 		StarRating = starRating;
 		ReviewCount = reviewCount;
 		this.reviewDate = reviewDate;
 		this.reviewContents = reviewContents;
-		this.reviewHeartConut = reviewHeartConut;
+		HeartConut = heartConut;
 		this.reviewStatus = reviewStatus;
 	}
 
-
-
-
-	
-	
-	
+	@Override
+	public String toString() {
+		return "Review [reviewNo=" + reviewNo + ", userId=" + userId + ", storeNo=" + storeNo + ", reviewTitle="
+				+ reviewTitle + ", StarRating=" + StarRating + ", ReviewCount=" + ReviewCount + ", reviewDate="
+				+ reviewDate + ", reviewContents=" + reviewContents + ", HeartConut=" + HeartConut + ", reviewStatus="
+				+ reviewStatus + "]";
+	}
 
 }
--- src/main/java/com/kh/bbang/review/service/logic/ReviewServiceImpl.java
+++ src/main/java/com/kh/bbang/review/service/logic/ReviewServiceImpl.java
@@ -2,11 +2,14 @@ package com.kh.bbang.review.service.logic;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.kh.bbang.review.domain.Review;
 import com.kh.bbang.review.service.ReviewService;
 import com.kh.bbang.review.store.ReviewStore;
 
+// @Service : 서비스 레이어, 내부에서 자바 로직을 처리
+@Service
 public class ReviewServiceImpl implements ReviewService{
 	// 의존성을 주입, 필요한 의존 객체의 “타입"에 해당하는 빈을 찾아 주입한다. *생성자* *setter* *필드*
 	@Autowired
@@ -19,6 +22,17 @@ public class ReviewServiceImpl implements ReviewService{
 	public int registerReview(Review review) {
 //		int result = rStore.
 		return 0;
+	}
+
+	@Override
+	public Review printDetailOneByNo(Integer reviewNo) {
+		Review review = rStore.selectOneByNo(session, reviewNo);
+		int result = 0;
+		if (review != null) {
+			result = rStore.updateReviewCount(session, reviewNo);
+			
+		}
+		return review;
 	}
 
 }
